5d279487c9e753c968bbfda4179eb9312383d2ff
Avei20 <muhammadabdulazizalghofari@gmail.com>
2023-03-27 23:45:04 +0700

    Initial Commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Heroes from "../components/Heroes.vue";
+import OurOrganizer from "../components/OurOrganizer.vue";
+import Feature from "../components/Feature.vue";
+import OurSpeaker from "../components/OurSpeaker.vue";</script>
+
+<template>
+  <Heroes/>
+
+  <Feature/>
+
+  <OurOrganizer/>
+
+  <OurSpeaker/>
+</template>
