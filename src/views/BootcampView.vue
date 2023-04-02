1efbc21e505471469987fd49ffde1fd1fe512b7c
Hary <noval.ramadan600@gmail.com>
2023-04-02 07:09:52 +0700

    section 2

--- src/views/BootcampView.vue
+++ src/views/BootcampView.vue
@@ -53,6 +53,35 @@
             </div>
         </div>
     </section>
+
+    <section class="w-full flex justify-center">
+        <div class="w-10/12 flex flex-row mt-10">
+            <div class="w-5/12">
+                <h1 class="text-5xl font-medium">What you will learn</h1>
+                <div class="w-full mt-10">
+                    <img src="/public/aej-logo.svg" class="w-11/12 bg-green-700  rounded-tr-[220px] h-[420px]" alt="AEJ Logo"/>
+                </div>
+            </div>
+            <div class="w-7/12 bg-red-400">
+                <div class="bg-blue-600">
+                    <div class="">
+                        <h1>Learn Android Architecture</h1>
+                        <p>4 months to complete</p>
+                        <p>Get hands-on experience by coding five real-world projects. Learn to build a route planner using OpenStreetMap data, write a process monitor for your computer, and implement your own smart pointers. Finally, showcase all your newfound skills by building a multithreaded traffic simulator and coding your own Learn Android application.</p>
+                    </div>
+                    <div class="mt-0 lg:mt-12 mb-8 lg:mb-24 space-y-4 sm:flex-row sm:justify-center">
+                        <a href="#" class="inline-flex mr-3 justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 focus:ring-gray-100">
+                            Enroll Now
+                            <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                        </a>
+                        <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-gray-300 focus:ring-4 focus:ring-gray-100">
+                            Download Syllabus
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
     
 </template>
 
