3afe3feafa3985f7da178b06033558f77bbf0f43
Avei20 <muhammadabdulazizalghofari@gmail.com>
2023-04-07 13:11:09 +0700

    Adding v-for in every catalog to looping in every array

--- src/views/CatalogView.vue
+++ src/views/CatalogView.vue
@@ -1,8 +1,3 @@
-<script setup>
-import Navbar from "../components/Navbar.vue";
-import { ref } from "vue";
-</script>
-
 <template>
 
     <section class="relative bg-gray-900">
@@ -52,7 +47,7 @@ import { ref } from "vue";
 
     <section class="w-full mt-10 flex justify-center">
         <div class="w-11/12 grid gap-5 grid-cols-2">
-            <div class="flex flex-row drop-shadow-2xl bg-gray-50">
+            <div class="flex flex-row drop-shadow-2xl bg-gray-50" v-for="catalogue in catalogueList" :key="catalogue.id">
                 <div class="border-r-2 p-3 w-60">
                     <div class="w-full">
                         <img src="/public/aej-logo.svg" class="w-36 max-md:w-full max-md:h-[300px] bg-green-700  rounded-bl-[55px] h-[130px]" alt="AEJ Logo"/>
@@ -69,9 +64,11 @@ import { ref } from "vue";
                 <div class="flex flex-col w-full items-center">
                     <div class="flex flex-col w-full items-center py-6">
                         <div class="flex flex-col w-11/12">
-                            <button class="bg-lime-400 px-4 py-2 rounded-full text-xs w-28">Nanodegree</button>
-                            <h1 class="text-xl font-semibold pt-4">Introduction to Programming</h1>
-                            <p class="text-xs pt-2">A first step for beginers towards careers in web and app development, machine learning, data science, AI and More.</p>
+                            <div>
+                                <button class="bg-lime-400 px-4 py-2 mr-2 rounded-full text-xs w-28" v-for="tag in catalogue.tags">{{ tag }}</button>
+                            </div>
+                            <h1 class="text-xl font-semibold pt-4">{{ catalogue.name }}</h1>
+                            <p class="text-xs pt-2">{{ catalogue.desc }}</p>
                         </div>
                     </div>
                     <div class="w-full bg-gray-200 flex justify-center mt-8">
@@ -79,11 +76,11 @@ import { ref } from "vue";
                                 <div class="flex items-center">
                                     <div class="flex items-center">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 640 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M576 0c17.7 0 32 14.3 32 32V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V32c0-17.7 14.3-32 32-32zM448 96c17.7 0 32 14.3 32 32V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V128c0-17.7 14.3-32 32-32zM352 224V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V224c0-17.7 14.3-32 32-32s32 14.3 32 32zM192 288c17.7 0 32 14.3 32 32V480c0 17.7-14.3 32-32 32s-32-14.3-32-32V320c0-17.7 14.3-32 32-32zM96 416v64c0 17.7-14.3 32-32 32s-32-14.3-32-32V416c0-17.7 14.3-32 32-32s32 14.3 32 32z"/></svg>
-                                        <p class="text-xs ml-2">Beginner</p>
+                                        <p class="text-xs ml-2">{{ catalogue.level }}</p>
                                     </div>
                                     <div class="flex items-center ml-2">
                                         <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
-                                        <p class="text-xs ml-2">4 Month</p>
+                                        <p class="text-xs ml-2">{{ catalogue.duration > 1 ? `${catalogue.duration} months.` : `${catalogue.duration} month` }}</p>
                                     </div>
                                 </div>
                     
@@ -93,16 +90,18 @@ import { ref } from "vue";
                                 <svg aria-hidden="true" class="w-5 h-5 text-black" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Third star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                                 <svg aria-hidden="true" class="w-5 h-5 text-black" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fourth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                                 <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
-                                <p class="text-xs ml-2">3866 reviews</p>
+                                <p class="text-xs ml-2">{{ catalogue.totalReview > 1 ? `${catalogue.totalReview} reviews` : `${catalogue.totalReview} review`  }}</p>
                             </div>
                         </div>
                     </div>
                     <div class="flex justify-center w-full">
                         <div class="flex flex-col w-11/12 py-6">
                             <h1 class="text-sm font-medium">Skill Covered</h1>
-                            <p class="text-xs pt-2">Front-End Development Basics, HTML, CSS, Python syntax, Text Processing in Python, DevTools, Command Line Interface Basics, Code Debugging Basics Java Scripts</p>
+                            <p class="text-xs pt-2">
+                                <span v-for="skill in catalogue.skillsCovered ">{{ skill == catalogue.skillsCovered[catalogue.skillsCovered.length - 2] ? `${skill}, ` : `${skill}.` }} </span>
+                            </p>
                             <h1 class="text-sm font-medium pt-2">Prerequisites</h1>
-                            <p class="text-xs pt-2">No Coding experience needed</p>
+                            <p class="text-xs pt-2">{{ catalogue.prerequites == "" ? 'No Coding experience needed.' : catalogue.prerequites }}</p>
                         </div>
                     </div>
                 </div>
@@ -114,7 +113,79 @@ import { ref } from "vue";
     
 </template>
 
-<script>
+<script setup>
+
+import Navbar from "../components/Navbar.vue";
+import { ref } from "vue";
+
+const catalogueList = ref([
+    {
+        id: 1,
+        name: 'Introduction to Programming Beginner',
+        tags: ['Nanodegree', 'Double'],
+        desc: 'A first step for beginers towards careers in web and app development, machine learning, data science, AI and More.',
+        level: 'Beginner',
+        duration: '4',
+        totalReview: 3866,
+        skillsCovered: ['Front-End Development Basics', 'HTML', 'CSS', 'Python syntax', 'Text Processing in Python', 'DevTools', 'Command Line Interface Basics', 'Code Debugging', 'Basics Java Script'],
+        prerequites: '',
+    },
+    {
+        id: 2,
+        name: 'Introduction to Programming C#',
+        tags: ['Nanodegree'],
+        desc: 'oeatnuasoeuh antsoehuanteohu oatensuh aoetuaoetnuhaoe ustaoehu a',
+        level: 'Beginner',
+        duration: '4',
+        totalReview: 3866,
+        skillsCovered: ['Front-End Development Basics', 'HTML', 'CSS', 'Python syntax', 'Text Processing in Python', 'DevTools', 'Command Line Interface Basics', 'Code Debugging Basics Java Script'],
+        prerequites: '',
+    },
+    {
+        id: 3,
+        name: 'Introduction to Programming C#',
+        tags: ['Nanodegree'],
+        desc: 'oeatnuasoeuh antsoehuanteohu oatensuh aoetuaoetnuhaoe ustaoehu a',
+        level: 'Beginner',
+        duration: '4',
+        totalReview: 3866,
+        skillsCovered: ['Front-End Development Basics', 'HTML', 'CSS', 'Python syntax', 'Text Processing in Python', 'DevTools', 'Command Line Interface Basics', 'Code Debugging Basics Java Script'],
+        prerequites: '',
+    },
+    {
+        id: 4,
+        name: 'Introduction to Programming C#',
+        tags: ['Nanodegree'],
+        desc: 'oeatnuasoeuh antsoehuanteohu oatensuh aoetuaoetnuhaoe ustaoehu a',
+        level: 'Beginner',
+        duration: '4',
+        totalReview: 3866,
+        skillsCovered: ['Front-End Development Basics', 'HTML', 'CSS', 'Python syntax', 'Text Processing in Python', 'DevTools', 'Command Line Interface Basics', 'Code Debugging Basics Java Script'],
+        prerequites: '',
+    },
+    {
+        id: 5,
+        name: 'Introduction to Programming C#',
+        tags: ['Nanodegree'],
+        desc: 'oeatnuasoeuh antsoehuanteohu oatensuh aoetuaoetnuhaoe ustaoehu a',
+        level: 'Beginner',
+        duration: '4',
+        totalReview: 3866,
+        skillsCovered: ['Front-End Development Basics', 'HTML', 'CSS', 'Python syntax', 'Text Processing in Python', 'DevTools', 'Command Line Interface Basics', 'Code Debugging Basics Java Script'],
+        prerequites: '',
+    },
+    {
+        id: 6,
+        name: 'Introduction to Programming C#',
+        tags: ['Nanodegree'],
+        desc: 'oeatnuasoeuh antsoehuanteohu oatensuh aoetuaoetnuhaoe ustaoehu a',
+        level: 'Beginner',
+        duration: '4',
+        totalReview: 3866,
+        skillsCovered: ['Front-End Development Basics', 'HTML', 'CSS', 'Python syntax', 'Text Processing in Python', 'DevTools', 'Command Line Interface Basics', 'Code Debugging Basics Java Script'],
+        prerequites: '',
+    },
+])
 
 const selectedCities = ref();
 const topics = ref([
